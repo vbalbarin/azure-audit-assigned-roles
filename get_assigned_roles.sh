e34581ba8fe8a3a9c0156b627c330546a71a2c73
veb3 <vincent.balbarin@yale.edu>
2021-05-25 22:17:19 -0400

    Using more concise output

--- get_assigned_roles.sh
+++ get_assigned_roles.sh
@@ -4,6 +4,8 @@ function timestamp() {
   printf "%s" "$(date -u +'%Y-%d-%mT%H:%M:%SZ')"
 }
 
+rm *.json
+
 IFS=$'\n'
 set -f
 
@@ -24,11 +26,12 @@ az_ad_users=($(jq -r '.[] | select(.principalType=="User") | .principalName' ass
 az_ad_group=($(jq -r '.[] | select(.principalType=="Group") | .principalName' assignments.json))
 
 printf "%3s" | tr " " "-"; printf '\n'
-printf -- "- timestamp: %s\n\n" $(timestamp)
+printf -- "-\n"
+printf -- "  timestamp: %s\n" $(timestamp)
 printf -- "  subscription: %s\n" "$(echo ${az_subscription_json} | jq -r '.name')"
 printf -- "  id: %s\n" "$(echo ${az_subscription_json} | jq -r '.id')"
-printf -- "  tenant: %s\n\n" "$(echo ${az_subscription_json} | jq -r '.tenantId')"
-printf -- "  assignments:\n\n"
+printf -- "  tenant: %s\n" "$(echo ${az_subscription_json} | jq -r '.tenantId')"
+printf -- "  assignments:\n"
 
 for r in "${az_assigned_rolenames[@]}"; do
   printf -v q '[ .[] | select(.roleDefinitionName=="%s") ]' "${r}"
@@ -38,47 +41,50 @@ for r in "${az_assigned_rolenames[@]}"; do
   az_ad_sps=($(jq -r '.[] | select(.principalType=="ServicePrincipal") | .principalName' "${f}.json"))
   az_ad_groups=($(jq -r '.[] | select(.principalType=="Group") | .principalName' "${f}.json"))
 
-  printf -- "    - role: %s\n" ${r}
-  printf -- "      users:"
+  printf -- "  -\n"
+  printf -- "    role: %s\n" ${r}
+  printf -- "    users:"
   if [[ ${#az_ad_users[@]} = 0 ]]; then
   printf -- " []\n"
   else
   printf -- "\n"
   for u in ${az_ad_users[@]}; do
-  printf -- "      - %s\n" ${u}
+  printf -- "    - %s\n" ${u}
   done
   fi
-  printf -- "      service_principals:"
+  printf -- "    service_principals:"
   if [[ ${#az_ad_sps[@]} = 0 ]]; then
   printf -- " []\n"
   else
   printf -- "\n"
   for s in ${az_ad_sps[@]}; do
-  printf -- "      - %s\n" ${s}
+  printf -- "    - %s\n" ${s}
   done
   fi
-  printf -- "      groups:"
+  printf -- "    groups:"
   if [[ ${#az_ad_groups[@]} = 0 ]]; then
   printf -- " []\n"
   else
   printf -- "\n"
   for g in ${az_ad_groups[@]}; do
-  printf -- "      - %s:" ${g}
-  # printf -- "        members:"
+  printf -- "    -\n"
+  printf -- "      %s:" ${g}
   members=($(az ad group member list -g "${g}" --query '[].{userPrincipalName: userPrincipalName}' | jq -r '.[] | .userPrincipalName'))
   if [[ ${#members[@]} = 0 ]]; then
   printf -- " []\n"
   else
   printf -- "\n"
   for m in ${members[@]}; do
-  printf -- "        - %s\n" "${m}"
+  printf -- "      - %s\n" "${m}"
   done
   fi
   done
   fi
-  printf -- "\n"
+  # printf -- "\n"
 
 done
+
+printf "%3s" | tr " " "."; printf '\n'
 
 set +f
 unset IFS
