8a620ff8382bbf09bf193272dd550fcb38cb39a9
vbalbarin <vincent.balbarin@yale.edu>
2021-06-08 10:16:53 -0400

    Renaming cache variables to prepare for refactor

--- get_assigned_roles_mgmtgroup.sh
+++ get_assigned_roles_mgmtgroup.sh
@@ -52,38 +52,38 @@ function acct_mg_parent {
 IFS=$'\n'
 set -f
 
-now=$(date +%s)
-max_cache_age=1800 #s or 30min
-# max_cache_age=120 #s or 2min
-
 az_subscription_json=$(az account show --query "{name: name, id:id, tenantId: tenantId}")
 az_subscription=$(echo ${az_subscription_json} | jq -r '.name')
 
 
-account_cache_name="account-cache-*"
-account_caches=($(find . -type dir -name "${account_cache_name}"  -exec basename {} \;))
-new_cache="$(pwd)/account-cache-${now}"
+now=$(date +%s)
+max_cache_age=1800 #s or 30min
+# max_cache_age=120 #s or 2min
 
-if [[ $(( ${#account_caches[@]} )) -ne 1 ]]; then
+cache_dir="account-cache-*"
+caches=($(find . -type dir -name "${cache_dir}"  -exec basename {} \;))
+new_cache_dir="$(pwd)/account-cache-${now}"
+
+if [[ $(( ${#caches[@]} )) -ne 1 ]]; then
   printf "No valid cache found.\n"
-  find . -type d -name "${account_cache_name}" -exec rm -Rf "{}" \;
-  # mkdir -p "${new_cache}"
-  cache=$(acct_mg_cache "${new_cache}")
+  find . -type d -name "${cache_dir}" -exec rm -Rf "{}" \;
+  # mkdir -p "${new_cache_dir}"
+  cache=$(acct_mg_cache "${new_cache_dir}")
 else
-  printf "Found cache: %s\n" "${account_caches[0]}"
-  crt=$( echo ${account_caches[0]%%.*} | cut -d '-' -f 3)
+  printf "Found cache: %s\n" "${caches[0]}"
+  crt=$( echo ${caches[0]%%.*} | cut -d '-' -f 3)
   age=$(( ${now} - ${crt} ))
   if [[ ${age} -gt ${max_cache_age} ]]; then
     printf 'Cache created %d minutes ago. Recreating expired cache...\n' $(( ${age}/60 ))
-    rm -Rf "${account_caches[0]}"
-    cache=$(acct_mg_cache "${new_cache}")
+    rm -Rf "${caches[0]}"
+    cache=$(acct_mg_cache "${new_cache_dir}")
   else
     printf "Cache created %d m ago. Valid.\n" $(( ${age}/60 ))
-    cache="$(pwd)/${account_caches[0]}"
+    cache="$(pwd)/${caches[0]}"
   fi
 fi
-
 printf "Cache: %s\n" "${cache}"
+
 
 az_management_group_ids=($( jq -r ".[] | .id" "${cache}/management_groups.json"  ))
 
